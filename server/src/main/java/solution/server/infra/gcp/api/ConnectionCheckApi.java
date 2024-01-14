576d9ddc233b4fde8ff4c52fade9eee0e4dd2795
chosunghyun18 <krismon@naver.com>
2024-01-14 22:11:49 +0900

    feat : fix code with api response dto

--- server/src/main/java/solution/server/infra/gcp/api/ConnectionCheckApi.java
+++ server/src/main/java/solution/server/infra/gcp/api/ConnectionCheckApi.java
@@ -9,15 +9,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import solution.server.global.common.dto.ApiResponse;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/v1/test")
 @AllArgsConstructor
 public class ConnectionCheckApi {
 
+//    @GetMapping(value="/{testNumber}")
+//    @ResponseStatus(HttpStatus.OK)
+//    public ResponseEntity<Long> getConnectTestNumber(@PathVariable Long testNumber){
+//        return ResponseEntity.ok(testNumber);
+//    }
     @GetMapping(value="/{testNumber}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Long> getConnectTestNumber(@PathVariable Long testNumber){
-        return ResponseEntity.ok(testNumber);
+    public ApiResponse<Long> getConnectTestNumber(@PathVariable Long testNumber){
+        return ApiResponse.success(testNumber);
     }
 }
