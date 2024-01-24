2a1e190e4736c0a10e1d78551e4bec928ec3aa2a
CzDgB <201801223@hufs.ac.kr>
2024-01-24 11:54:06 +0900

    feat : add TypeTag Entity Repo Service

--- server/src/main/java/solution/server/typetag/model/TypeTag.java
+++ server/src/main/java/solution/server/typetag/model/TypeTag.java
@@ -1,7 +1,9 @@
 package solution.server.typetag.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import solution.server.tag.model.Tag;
 import solution.server.type.model.Type;
@@ -9,7 +11,7 @@ import solution.server.type.model.Type;
 import static jakarta.persistence.FetchType.LAZY;
 
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class TypeTag {
     @Id
@@ -24,4 +26,23 @@ public class TypeTag {
     @ManyToOne(fetch = LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "tag_id")
     private Tag tag;
+
+    public TypeTag updateType(Type type){
+        this.type = type;
+        return this;
+    }
+
+    public TypeTag updateTag(Tag tag){
+        this.tag = tag;
+        return this;
+    }
+
+    public String getTypeName(){return type.getName();}
+    public String getTagName(){return tag.getName();}
+
+    public TypeTag addInfo(Type type, Tag tag){
+        this.type = type;
+        this.tag = tag;
+        return this;
+    }
 }
